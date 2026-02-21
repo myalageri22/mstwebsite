97f3c412b55a3d0fe3f604c3843f4ffd19d3413c
BobaCattt <jack2020cheng@gmail.com>
2026-02-21 17:03:45 -0600

    Updated Jack bio 2x

--- src/data/tutors.ts
+++ src/data/tutors.ts
@@ -37,7 +37,7 @@ export const tutors: Tutor[] = [
     fullBio:
       "Jack is a current 10th grader at Tompkins High School and has a strong background in academic competitions, with mutiple awards at various levels. On the side he is also a professional table tennis player.",
     image: "/tutors/maya.svg",
-    badges: ["3x Science Olympiad Invitational Medals", "Tompkins HS Class of 2027-2028 Rank 2", "2021 AMC8 Achievement & Honor Roll", "3x TMSCA State Medals", "4th Place Team at State (2024)"]
+    badges: ["3x Science Olympiad Invitational Medals", "Tompkins HS Class of 2027-2028 Current Salutatorian", "2021 AMC8 Achievement & Honor Roll", "3x TMSCA State Medals", "4th Place Team at State (2024)"]
   },
   {
     id: "eric-lin",
